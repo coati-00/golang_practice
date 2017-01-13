5fbabda6042891459133e54340a02d6211652fb7
Cassandra Dunlop <coati.00@gmail.com>
2017-01-13 16:44:33 -0500

    start going through go blueprints book

--- sum_of_array.go
+++ sum_of_array.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
-import "strings"
-import "strconv"
+import (
+    "fmt"
+    "strings"
+    "strconv"
+)
 
 
 func StrToInt(str string) (int, error) {
@@ -13,19 +15,13 @@ func StrToInt(str string) (int, error) {
 
 func main() {
 	someString := "1 2 3 4 5 6"
-	
 	words := strings.Fields(someString)
 	
 	var total int;
 	var intword int;
-
-	fmt.Println(words, len(words))
 	
 	for i := 0; i < len(words); i++ {
-	    fmt.Println(words[i])
-	    //var intword int = int(words[i])
 	    intword, _ = StrToInt(words[i])
-	    fmt.Println(intword)
 	    total = intword + total
 	}
 	fmt.Println(total)
